a4b7e6cf9083701a37c1684241df01b6aa789202
Gumaro Contreras <gumaroconter123@hotmail.com>
2020-04-02 17:37:48 -0600

    conexion

--- Ramirez Boxing/src/Ventanas/conectar.java
+++ Ramirez Boxing/src/Ventanas/conectar.java
@@ -5,10 +5,26 @@
  */
 package Ventanas;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author gumaro
  */
 public class conectar {
+    
+    Connection conect = null;
+    public Connection conexion(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+             conect =DriverManager.getConnection("jdbc:mysql://localhost/ramirezboxingclub", "root", "1234");
+        } catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error"+e);
+        }
+        return conect;
+    }
     
 }
